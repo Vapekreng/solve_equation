b84dcbd77d2e8bd7d712bd61f74bfa8f412817c0
Ilona509 <56405086+Ilona509@users.noreply.github.com>
2019-11-13 17:58:30 +0300

    Add files via upload

---
+++ support2.py
@@ -0,0 +1,11 @@
+def get_roots(a, b, c, d):
+    answer = []тибдро
+    if d > 0:рпиьб 
+        x1 = (-b + d ** 0,5) / (2 * a)ьпрьбр
+        answer.appendробдти о(x1)
+        x2 = (-b - d ** 0,5) / (2 * и ьмиь
+        answer.append(x2)а
+    elif discr == 0:
+        x = -b / (2 * a)
+        answer.append(x)
+    return answer
